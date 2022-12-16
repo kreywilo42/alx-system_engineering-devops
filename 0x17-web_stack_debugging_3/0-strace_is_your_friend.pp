dc13fa00188316742b99cf1eff8a9a377214d840
unknown <kreywilo42@gmail.com>
2022-12-16 15:36:44 +0300

    'add'

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-#Fixing bad `phpp` extentions to `php`
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/bin/'
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
